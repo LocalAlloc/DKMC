afcfdc1fb6639644e6eb1efda91f2fa336582bc0
NightFall <72191792+Maruf132@users.noreply.github.com>
2022-03-30 21:12:44 +0530

    Update ui.py

--- core/ui.py
+++ core/ui.py
@@ -11,20 +11,20 @@ class UI:
         
     def banner(self):
         self.clearscreen()
-        print "\n\n\033[33mDKMC - Don't kill my cat"
-        print "         Evasion tool - Mr.Un1k0d3r RingZer0 Team\033[00m"
-        print "     |\      _,,,---,,_"
-        print "    /,`.-'`'    -.  ;-;;,_"
-        print "   |,4-  ) )-,_..;\ (  `'-'"
-        print "  '---''(_/--'  `-'\_)    The sleepy cat"
-        print "\n----------------------------------------------------"
+        print ("\n\n\033[33mDKMC - Don't kill my cat")
+        print ("         Evasion tool - Mr.Un1k0d3r RingZer0 Team\033[00m")
+        print ("     |\      _,,,---,,_")
+        print ("    /,`.-'`'    -.  ;-;;,_")
+        print ("   |,4-  ) )-,_..;\ (  `'-'")
+        print ("  '---''(_/--'  `-'\_)    The sleepy cat")
+        print ("\n----------------------------------------------------")
 
     def show_menu(self, options, error = ""):
-        print "Select an option:\n"
+        print ("Select an option:\n")
         for option in options:
-            print "\t[*] (%s)\t%s" % (option)
+            print ("\t[*] (%s)\t%s" % (option))
         if not error == "":
-            print "\n\033[91m[-] >>> %s\033[00m" % error
+            print ("\n\033[91m[-] >>> %s\033[00m" % error)
         return self.capture_input()  
       
     def capture_input(self, mod = ""):
@@ -33,10 +33,10 @@ class UI:
         return raw_input("\n%s>>> " % mod).strip()
     
     def print_msg(self, msg):
-        print "\t\033[32m[+] %s\033[00m" % msg
+        print ("\t\033[32m[+] %s\033[00m" % msg)
         
     def print_error(self, error):
-        print "\033[91m[-] >>> %s\033[00m" % error
+        print ("\033[91m[-] >>> %s\033[00m" % error)
         
     def print_debug(self, msg):
-        print "\033[36m[DEBUG] >>> %s\033[00m" % msg
+        print ("\033[36m[DEBUG] >>> %s\033[00m" % msg)
